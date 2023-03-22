0d6998ab1de410f9072bd7a791e55112cf4bfa23
DCK <dogancihatkayanews@gmail.com>
2023-03-23 02:13:57 +0300

    apiler ile ilgili güncelleme yapıldı v1.3 klinikler eklendi

--- resources/views/admin/doctor/edit.blade.php
+++ resources/views/admin/doctor/edit.blade.php
@@ -66,7 +66,7 @@
                                         </div>
                                         <div class="col-lg-12 col-md-12 col-sm-12 form-group">
                                             <label>Adres</label>
-                                            <textarea name="address" value="{{ $doctor->klinik }}"></textarea>
+                                            <textarea name="address" value="{{ $doctor->address }}">{{ $doctor->address }}</textarea>
                                         </div>
                                     </div>
 
